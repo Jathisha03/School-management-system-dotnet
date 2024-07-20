e689f67e07b782f135ea59f3f529fc96891eb140
Suthaharan Jathisha <128876489+Jathisha03@users.noreply.github.com>
2024-07-20 10:34:48 +0530

    Grade Save Button

--- Student registration/Student Registration/Student Registration/Grade List Form.cs
+++ Student registration/Student Registration/Student Registration/Grade List Form.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace Student_Registration
 {
     public partial class Grade_List_Form : Form
     {
+        string connectionString = "Data Source=DESKTOP-S3E7KHQ\\SQLEXPRESS;Initial Catalog= StudentRegistration;Trusted_Connection=True";
+
         public Grade_List_Form()
         {
             InitializeComponent();
@@ -19,7 +22,30 @@ namespace Student_Registration
 
         private void btnGradeSave_Click(object sender, EventArgs e)
         {
+            string grade_name = txtGradeName.Text;
+            string grade_group = txtGradeGroup.Text;
+            string grade_order = txtGradeOrder.Text;
+            
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+                SqlCommand cmd = new SqlCommand($"insert into grades (grade_name,grade_group,grade_order) values('{grade_name}','{grade_group}','{grade_order}')", con);
+                //SqlCommand cmd = new SqlCommand($"insert into students (admission_no,first_name,last_name,full_name,gender,date_of_birth,stu_nic_no,tp_no,grade_id,medium,date_of_admission,resident_address) values('{txtStudentAdmissionNo.Text }','{ txtStudentFirstName.Text}','{txtStudentLastname.Text}','{txtStudentFullName.Text}','{gender}','{dtStudentDateOfBirth.Text}','{txtStudentStuNicNo.Text}','{txtStudentPhoneNo.Text}','{comStudentGradeId.Text}','{txtStudentMedium.Text}','{dtStudentDateOfAdmission.Text}','{txtStudentAddress.Text}')", con);
 
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Save successfully");
+
+                cmd.Dispose();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:  " + ex.Message);
+                throw;
+
+            }
         }
     }
 }
